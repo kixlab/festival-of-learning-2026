ca62e6d993d6d0951430e74cfa39875c2898e370
eunyoungko <eunyoung4444@gmail.com>
2025-06-11 18:07:32 +0900

    Fix image loading in GitHub Pages

--- next.config.js
+++ next.config.js
@@ -3,8 +3,15 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
   basePath: process.env.NODE_ENV === 'production' ? '/festival-of-learning-2026' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/festival-of-learning-2026/' : '',
 };
 
 module.exports = nextConfig; 
