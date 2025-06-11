b96e9206ca669912e1e9b0606cd5f267f47e3d75
eunyoungko <eunyoung4444@gmail.com>
2025-06-11 18:09:44 +0900

    Fix image paths for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -3,15 +3,9 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/festival-of-learning-2026' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/festival-of-learning-2026/' : '',
+  basePath: '/festival-of-learning-2026',
+  assetPrefix: '/festival-of-learning-2026/',
 };
 
 module.exports = nextConfig; 
