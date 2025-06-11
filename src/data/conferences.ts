756a7be22e86d8a443bd9b4f6f91a4983d7c5a80
eunyoungko <eunyoung4444@gmail.com>
2025-06-12 00:17:05 +0900

    Fix image paths for GitHub Pages

--- src/data/conferences.ts
+++ src/data/conferences.ts
@@ -18,9 +18,9 @@ export const conferences: Conference[] = [
     description: 'A premier conference focusing on the latest developments in educational technology and its applications in teaching and learning.',
     website: '#',
     dates: {
-      paperSubmission: 'January 15, 2026',
-      notification: 'March 15, 2026',
-      cameraReady: 'April 15, 2026',
+      paperSubmission: 'TBD',
+      notification: 'TBD',
+      cameraReady: 'TBD',
       conference: 'June 29 - July 3, 2026'
     }
   },
@@ -30,9 +30,9 @@ export const conferences: Conference[] = [
     description: 'A leading conference exploring the intersection of artificial intelligence and education.',
     website: '#',
     dates: {
-      paperSubmission: 'January 20, 2026',
-      notification: 'March 20, 2026',
-      cameraReady: 'April 20, 2026',
+      paperSubmission: 'TBD',
+      notification: 'TBD',
+      cameraReady: 'TBD',
       conference: 'June 29 - July 3, 2026'
     }
   },
@@ -42,10 +42,22 @@ export const conferences: Conference[] = [
     description: 'A major conference on data mining and analytics in educational settings.',
     website: '#',
     dates: {
-      paperSubmission: 'January 25, 2026',
-      notification: 'March 25, 2026',
-      cameraReady: 'April 25, 2026',
+      paperSubmission: 'TBD',
+      notification: 'TBD',
+      cameraReady: 'TBD',
       conference: 'June 29 - July 3, 2026'
     }
-  }
+  },
+  {
+    id: 'isls',
+    name: 'International Society of the Learning Sciences Annual Meeting 2026',
+    description: 'A premier conference focusing on the latest developments in educational technology and its applications in teaching and learning.',
+    website: '#',
+    dates: {
+      paperSubmission: 'TBD',
+      notification: 'TBD',
+      cameraReady: 'TBD',
+      conference: 'June 29 - July 3, 2026'
+    }
+  },
 ]; 
